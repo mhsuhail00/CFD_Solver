e1de9970877e0daf49a72efdcea1d35cc4dc8467
mhsuhail00 <mhsuhail00@gmail.com>
2025-08-25 18:43:57 +0530

    Added more code

--- solver.cpp
+++ solver.cpp
@@ -9,181 +9,181 @@ string INPUT_FILE = "INP.DAT";
 
 class Solver {
 public:
+    static const int np1 = 350;
+    static const int np2 = 570;
+
+    // 2D coefficient matrices (pressure equation)
+    double ae[np1][np2];
+    double aw[np1][np2];
+    double as[np1][np2];
+    double an[np1][np2];
+    double ase[np1][np2];
+    double ane[np1][np2];
+    double asw[np1][np2];
+    double anw[np1][np2];
+    double ap[np1][np2];
+
+    double alph[np1][np2], beta[np1][np2], gamma[np1][np2];
+    string filnam[100], resfile;
+
+    // 2D velocity coefficient matrices (au* series)
+    double aue[np1][np2];
+    double auw[np1][np2];
+    double aun[np1][np2];
+    double aus[np1][np2];
+    double aune[np1][np2];
+    double ause[np1][np2];
+    double ausw[np1][np2];
+    double aunw[np1][np2];
+    double aup[np1][np2];
+
+    // 2D temperature coefficient matrices (at* series)
+    double ate[np1][np2];
+    double atw[np1][np2];
+    double atn[np1][np2];
+    double ats[np1][np2];
+    double atne[np1][np2];
+    double atse[np1][np2];
+    double atsw[np1][np2];
+    double atnw[np1][np2];
+    double atp[np1][np2];
+
+    // 1D boundary coefficient arrays (b* series)
+    double bus[np1];
+    double buse[np1];
+    double busw[np1];
+    double bts[np1];
+    double btse[np1];
+    double btsw[np1];
+    double bun[np1];
+    double bune[np1];
+    double bunw[np1];
+    double btn[np1];
+    double btne[np1];
+    double btnw[np1];
+
+    // 2D higher-order velocity coefficient matrices (au** series)
+    double aunn[np1][np2];
+    double auss[np1][np2];
+    double auee[np1][np2];
+    double auww[np1][np2];
+    double aunnee[np1][np2];
+    double aunnww[np1][np2];
+    double aussee[np1][np2];
+    double aussww[np1][np2];
+    double aunne[np1][np2];
+    double aunnw[np1][np2];
+    double ausse[np1][np2];
+    double aussw[np1][np2];
+    double aunee[np1][np2];
+    double aunww[np1][np2];
+    double ausee[np1][np2];
+    double ausww[np1][np2];
+    double auup[np1][np2];
+
+    // 2D higher-order temperature coefficient matrices (at** series)
+    double atnn[np1][np2];
+    double atss[np1][np2];
+    double atee[np1][np2];
+    double atww[np1][np2];
+    double atnnee[np1][np2];
+    double atnnww[np1][np2];
+    double atssee[np1][np2];
+    double atssww[np1][np2];
+    double atnne[np1][np2];
+    double atnnw[np1][np2];
+    double atsse[np1][np2];
+    double atssw[np1][np2];
+    double atnee[np1][np2];
+    double atnww[np1][np2];
+    double atsee[np1][np2];
+    double atsww[np1][np2];
+    double atup[np1][np2];
+
+    // 2D grid and transformation arrays
+    double ajac[np1][np2];
+    double dxix[np1][np2];
+    double dxiy[np1][np2];
+    double dex[np1][np2];
+    double dey[np1][np2];
+    double q[np1][np2];
+    double si[np1][np2];
+    double dil[np1][np2];
+    double qup[np1][np2];
+    double qvp[np1][np2];
+    double qu[np1][np2];
+    double qv[np1][np2];
+    double qt[np1][np2];
+    double p1[np1][np2];
+    double q1[np1][np2];
+    double sol[np1][np2];
+    double pcor[np1][np2];
+    double p[np1][np2];
+    double uxi[np1][np2];
+    double uet[np1][np2];
+    double vort[np1][np2];
+
+    // 3D arrays
+    double x[2][np1][np2];
+    double u[3][np1][np2];
+    double us[2][np1][np2];
+    double h[3][np1][np2];
+    double up[3][np1][np2];
+    double uold[3][np1][np2];
+
+    // 2D boundary velocity arrays
+    double vr[2][np1];
+    double vth[2][np1];
+
+    // 1D arrays
+    double dxi[2];
+    double xnox[np1];
+    double xnix[np1];
+    double xnoy[np1];
+    double xniy[np1];
+    double xnixi[np1];
+    double xnoxi[np1];
+    double xniet[np1];
+    double xnoet[np1];
+    double d2u[3];
+    double conv[3];
+    double vdotn[np1];
+    double thi[np1];
+    double alc[3];
+
+    // Scalar variables (REAL*8 declarations)
+    double Nuss, p_grid, a_grid, ar, aaa, sgn, f_ar;
+
+    // Physical parameters (double due to implicit REAL*8)
+    double Ri = 0.0;                                    // Richardson number
+    double F = 0.0;                                     // Frequency
+    double Pr = 0.71;                                   // Prandtl number
+    double Pi = acos(-1.0);                             // Pi constant
+    double thetamax = Pi/12.0;                          // Maximum angle
+    double speed_amp = thetamax * 2.0 * Pi * F;         // Speed amplitude
+    double accn_amp = 2.0 * Pi * F * speed_amp;         // Acceleration amplitude
+
+    // Flow conditions
+    double alpha = 82.0;                                // Angle from gravity vector
+    double uinf = sin(alpha * Pi / 180.0);              // Free stream u-velocity
+    double vinf = cos(alpha * Pi / 180.0);              // Free stream v-velocity
+    double Re = 1000.0;                                 // Reynolds number
+    double ubar = 0.05;                                 // Characteristic velocity
+    double dt = 0.01e-2;                                // Time step (0.0001)
+    double eps = 1e-2;                                  // Convergence tolerance
+
+    // Control parameters (integer due to implicit rule for i,j,k,l,m,n)
+    int norm = 0;                                       // Normalization flag
+    int MAXSTEP = 5000000;                              // Maximum time steps
+    int restart = 0;                                    // Restart flag (changed from 0 to 1)
+    int nsnap = 0;                                      // Current snapshot number
+    int maxsnap = 100;                                  // Maximum snapshots
+    int iflag = 1;
     Solver() {
-        const int np1=350, np2=570;
+        // dummy variables
+        int ic1, ic2, ic3, ic4, bbb, irem;
 
-        // 2D coefficient matrices (pressure equation)
-        double ae[np1][np2];
-        double aw[np1][np2];
-        double as[np1][np2];
-        double an[np1][np2];
-        double ase[np1][np2];
-        double ane[np1][np2];
-        double asw[np1][np2];
-        double anw[np1][np2];
-        double ap[np1][np2];
-
-        double alph[np1][np2], beta[2][np1][np2], gamma[np1][np2];
-        string filnam[100], resfile;
-
-        string filnam[100], resfile;
-
-        // 2D velocity coefficient matrices (au* series)
-        double aue[np1][np2];
-        double auw[np1][np2];
-        double aun[np1][np2];
-        double aus[np1][np2];
-        double aune[np1][np2];
-        double ause[np1][np2];
-        double ausw[np1][np2];
-        double aunw[np1][np2];
-        double aup[np1][np2];
-
-        // 2D temperature coefficient matrices (at* series)
-        double ate[np1][np2];
-        double atw[np1][np2];
-        double atn[np1][np2];
-        double ats[np1][np2];
-        double atne[np1][np2];
-        double atse[np1][np2];
-        double atsw[np1][np2];
-        double atnw[np1][np2];
-        double atp[np1][np2];
-
-        // 1D boundary coefficient arrays (b* series)
-        double bus[np1];
-        double buse[np1];
-        double busw[np1];
-        double bts[np1];
-        double btse[np1];
-        double btsw[np1];
-        double bun[np1];
-        double bune[np1];
-        double bunw[np1];
-        double btn[np1];
-        double btne[np1];
-        double btnw[np1];
-
-        // 2D higher-order velocity coefficient matrices (au** series)
-        double aunn[np1][np2];
-        double auss[np1][np2];
-        double auee[np1][np2];
-        double auww[np1][np2];
-        double aunnee[np1][np2];
-        double aunnww[np1][np2];
-        double aussee[np1][np2];
-        double aussww[np1][np2];
-        double aunne[np1][np2];
-        double aunnw[np1][np2];
-        double ausse[np1][np2];
-        double aussw[np1][np2];
-        double aunee[np1][np2];
-        double aunww[np1][np2];
-        double ausee[np1][np2];
-        double ausww[np1][np2];
-        double auup[np1][np2];
-
-        // 2D higher-order temperature coefficient matrices (at** series)
-        double atnn[np1][np2];
-        double atss[np1][np2];
-        double atee[np1][np2];
-        double atww[np1][np2];
-        double atnnee[np1][np2];
-        double atnnww[np1][np2];
-        double atssee[np1][np2];
-        double atssww[np1][np2];
-        double atnne[np1][np2];
-        double atnnw[np1][np2];
-        double atsse[np1][np2];
-        double atssw[np1][np2];
-        double atnee[np1][np2];
-        double atnww[np1][np2];
-        double atsee[np1][np2];
-        double atsww[np1][np2];
-        double atup[np1][np2];
-
-        // 2D grid and transformation arrays
-        double ajac[np1][np2];
-        double dxix[np1][np2];
-        double dxiy[np1][np2];
-        double dex[np1][np2];
-        double dey[np1][np2];
-        double q[np1][np2];
-        double si[np1][np2];
-        double dil[np1][np2];
-        double qup[np1][np2];
-        double qvp[np1][np2];
-        double qu[np1][np2];
-        double qv[np1][np2];
-        double qt[np1][np2];
-        double p1[np1][np2];
-        double q1[np1][np2];
-        double sol[np1][np2];
-        double pcor[np1][np2];
-        double p[np1][np2];
-        double uxi[np1][np2];
-        double uet[np1][np2];
-        double vort[np1][np2];
-
-        // 3D arrays
-        double x[2][np1][np2];
-        double u[3][np1][np2];
-        double us[2][np1][np2];
-        double h[3][np1][np2];
-        double up[3][np1][np2];
-        double uold[3][np1][np2];
-
-        // 2D boundary velocity arrays
-        double vr[2][np1];
-        double vth[2][np1];
-
-        // 1D arrays
-        double dxi[2];
-        double xnox[np1];
-        double xnix[np1];
-        double xnoy[np1];
-        double xniy[np1];
-        double xnixi[np1];
-        double xnoxi[np1];
-        double xniet[np1];
-        double xnoet[np1];
-        double d2u[3];
-        double conv[3];
-        double vdotn[np1];
-        double thi[np1];
-        double alc[3];
-
-        // Scalar variables (REAL*8 declarations)
-        double Nuss, p_grid, a_grid, ar, aaa, sgn, f_ar;
-
-        // Physical parameters (double due to implicit REAL*8)
-        double Ri = 0.0;                                    // Richardson number
-        double F = 0.0;                                     // Frequency
-        double Pr = 0.71;                                   // Prandtl number
-        double Pi = acos(-1.0);                             // Pi constant
-        double thetamax = Pi/12.0;                          // Maximum angle
-        double speed_amp = thetamax * 2.0 * Pi * F;         // Speed amplitude
-        double accn_amp = 2.0 * Pi * F * speed_amp;         // Acceleration amplitude
-
-        // Flow conditions
-        double alpha = 82.0;                                // Angle from gravity vector
-        double uinf = sin(alpha * Pi / 180.0);              // Free stream u-velocity
-        double vinf = cos(alpha * Pi / 180.0);              // Free stream v-velocity
-        double Re = 1000.0;                                 // Reynolds number
-        double ubar = 0.05;                                 // Characteristic velocity
-        double dt = 0.01e-2;                                // Time step (0.0001)
-        double eps = 1e-2;                                  // Convergence tolerance
-
-        // Control parameters (integer due to implicit rule for i,j,k,l,m,n)
-        int norm = 0;                                       // Normalization flag
-        int MAXSTEP = 5000000;                              // Maximum time steps
-        int restart = 0;                                    // Restart flag (changed from 0 to 1)
-        int nsnap = 0;                                      // Current snapshot number
-        int maxsnap = 100;                                  // Maximum snapshots
-        int iflag = 1; 
-    }
-    // Read input file and initialize variables
-    void initialize() {
+        // Read input file and initialize variables
         ifstream input_file(INPUT_FILE);
         if(!input_file) {
             cerr << "Error opening input file: " << INPUT_FILE << endl;
@@ -191,7 +191,88 @@ public:
         }
         cout << "Input file opened successfully." << endl;
 
+        input_file >> n[0] >> n[1] >> dxi[0] >> dxi[1];
+        input_file >> p_grid >> a_grid >> ar;
+        input_file >> ic1 >> ic2 >> ic3 >> ic4;
 
+        for (int j = 0; j < n[1]; j++) {
+            for (int i = 0; i < n[0]; i++) {
+                input_file >> aaa >> bbb >> x[0][i][j] >> x[1][i][j];
+            }
+        }
+
+        for (int j = 0; j < n[1]; j++) {
+            for (int i = 0; i < n[0]; i++) {
+                input_file >> dxix[i][j] >> dxiy[i][j] >> dex[i][j] >> dey[i][j];
+            }
+        }
+
+        for (int j = 0; j < n[1]; j++) {
+            for (int i = 0; i < n[0]; i++) {
+                input_file >> alph[i][j] >> beta[i][j] >> gamma[i][j];
+            }
+        }
+
+        for (int j = 0; j < n[1]; j++) {
+            for (int i = 0; i < n[0]; i++) {
+                input_file >> ajac[i][j];
+            }
+        }
+
+        for (int j = 0; j < n[1]; j++) {
+            for (int i = 0; i < n[0]; i++) {
+                input_file >> xnix[i] >> xniy[i] >> xnox[i] >> xnoy[i];
+            }
+        }
+
+        for (int j = 0; j < n[1]; j++) {
+            for (int i = 0; i < n[0]; i++) {
+                p1[i][j] = 0.0;
+                q1[i][j] = 0.0;
+            }
+        }
+
+        irem = 0;
+        n[1] = n[1] - irem;
+        if (irem != 0) {
+            for (int i = 0; i < n[0]; i++) {
+                xnox[i] = -dex[i][n[1]] / sqrt(gamma[i][n[1]]);
+                xnoy[i] = -dey[i][n[1]] / sqrt(gamma[i][n[1]]);
+            }
+        }
+
+        // --------------------------------------------------------
+        // generating filenames for saving the snapshots
+        // --------------------------------------------------------
+        for (int i = 0; i < maxsnap; i++) {
+            filnam[i] = "SNAP000.DAT";
+        }
+
+        int i3, i2, i1;
+        for (int k = 0; k < maxsnap; k++) {
+            i3 = k / 100;
+            i2 = (k - 100 * i3) / 10;
+            i1 = k - i2 * 10 - i3 * 100;
+            filnam[k][5] = '0' + i3;
+            filnam[k][6] = '0' + i2;
+            filnam[k][7] = '0' + i1;
+        }
+
+        // --------------------------------------------------------
+        // CALCULATING NXi AND Net AT OUTER AND INNER POINTS
+        // --------------------------------------------------------
+        // at inner first
+        int j = 1;
+        for (int i = 0; i < n[0]; i++) {
+            xnixi[i] = dxix[i][j] * xnix[i] + dxiy[i][j] * xniy[i];
+            xniet[i] = dex[i][j] * xnix[i] + dey[i][j] * xniy[i];
+        }
+
+        j = n[1];
+        for (int i = 0; i < n[0]; i++) {
+            xnoxi[i] = dxix[i][j] * xnox[i] + dxiy[i][j] * xnoy[i];
+            xnoet[i] = dex[i][j] * xnox[i] + dey[i][j] * xnoy[i];
+        }
     }
 
 };
